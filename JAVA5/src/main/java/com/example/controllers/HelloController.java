68b64ebe1c0ca636e652f77fdfc78e3696ee1b48
MrBom-05 <herogamings969@gmail.com>
2023-05-12 15:05:49 +0700

    new

---
+++ JAVA5/src/main/java/com/example/controllers/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+//    @RequestMapping(value = "hello", method = RequestMethod.GET)
+//    @PostMapping
+
+    @GetMapping("hello")
+    public String sayHello(Model model) {
+        model.addAttribute("name", "trungdt2");
+        return "hello";
+    }
+}
---
+++ JAVA5/src/main/webapp/views/hello.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" pageEncoding="utf-8" %>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+</head>
+<body>
+<h1>Hello ${name}</h1>
+<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
